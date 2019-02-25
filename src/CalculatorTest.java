fb5783fe974ee0659d9d86726641a6126e490e5f
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-02-25 23:52:15 +0900

    2장. 테스트 코드의 분리

---
+++ src/CalculatorTest.java
@@ -0,0 +1,9 @@
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.add(3, 9));
+        System.out.println(calculator.subtract(3, 9));
+        System.out.println(calculator.multiply(3, 9));
+        System.out.println(calculator.divide(3, 9));
+    }
+}
