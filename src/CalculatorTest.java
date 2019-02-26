927bb65a5d469669026426d476244dadfa953125
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-02-26 22:13:22 +0900

    2.

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,28 +1,31 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
 
     @Test
     public void add() {
         Calculator cal = new Calculator();
-        System.out.println(cal.add(6,3));
+        assertEquals("더하기 결과 오류",9,cal.add(6,3));
     }
 
     @Test
     public void subtract() {
         Calculator cal = new Calculator();
-        System.out.println(cal.subtract(6,3));
-    }
-
-    @Test
-    public void devide() {
-        Calculator cal = new Calculator();
-        System.out.println(cal.divide(6,3));
+        assertEquals("뺄샘 결과 오류 ",3,cal.subtract(6,3));
     }
 
     @Test
     public void multiply() {
         Calculator cal = new Calculator();
-        System.out.println(cal.multiply(6,3));
+        assertEquals("곱셉 결과 오류 ",18 ,cal.multiply(6,3));
     }
+
+    @Test
+    public void devide() {
+        Calculator cal = new Calculator();
+        assertEquals("나눗셈 결과 오류 ",2, cal.divide(6,3));
+    }
+
 }
