8aa83336dc67ac772a00a3f5cba75ad67a4255be
DESKTOP-49C2M5J\LEE <leechang0423@naver.com>
2019-02-26 22:21:28 +0900

    2장.  중복소스 제거

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,31 +1,41 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator cal;
+
+    @Before
+    public void setup() {
+        cal = new Calculator();
+    }
+
     @Test
     public void add() {
-        Calculator cal = new Calculator();
         assertEquals("더하기 결과 오류",9,cal.add(6,3));
     }
 
     @Test
     public void subtract() {
-        Calculator cal = new Calculator();
         assertEquals("뺄샘 결과 오류 ",3,cal.subtract(6,3));
     }
 
     @Test
     public void multiply() {
-        Calculator cal = new Calculator();
         assertEquals("곱셉 결과 오류 ",18 ,cal.multiply(6,3));
     }
 
     @Test
     public void devide() {
-        Calculator cal = new Calculator();
         assertEquals("나눗셈 결과 오류 ",2, cal.divide(6,3));
+    }
+
+    @After
+    public void teardown() {
+
     }
 
 }
